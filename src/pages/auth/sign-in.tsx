f166e5012de6483c1e2f5170a95de55146dee4e5
Caique Morales <caiquemorales20@gmail.com>
2024-06-21 16:56:26 -0300

    feat: form validation in login with react-hook-form + zod

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,33 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
+const signInSchema = z.object({
+  email: z.string().min(1).email(),
+})
+
+type SignInType = z.infer<typeof signInSchema>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInType>({
+    resolver: zodResolver(signInSchema),
+  })
+
+  async function handleSignIn(data: SignInType) {
+    console.log(data)
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -19,12 +42,17 @@ export function SignIn() {
             </p>
           </header>
 
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input
+                {...register('email')}
+                autoComplete="email"
+                id="email"
+                type="email"
+              />
             </div>
-            <Button className="w-full" type="submit">
+            <Button disabled={isSubmitting} className="w-full" type="submit">
               Acessar painel
             </Button>
           </form>
