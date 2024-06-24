175b791fa0fe17588c99f56ef8277b5e84dd1719
Caique Morales <caiquemorales20@gmail.com>
2024-06-23 22:32:14 -0300

    feat: header + orders table + orders filter + dark/light theme

--- src/app.tsx
+++ src/app.tsx
@@ -3,15 +3,18 @@ import './globals.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/sonner'
 import { router } from './router'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Pizza Trends" />
-      <RouterProvider router={router} />
-      <Toaster />
+      <ThemeProvider storageKey="pizzatrends-theme">
+        <Helmet titleTemplate="%s | Pizza Trends" />
+        <RouterProvider router={router} />
+        <Toaster />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
